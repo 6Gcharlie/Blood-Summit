20966b5fa921c4b77b934c442566b3adca536f35
Charlie George-Carter <charlie.george-carter@fluidbranding.com>
2023-06-07 19:28:22 +0100

    Resolved a few "Grunt" todos

--- assets/scripts/grunt.py
+++ assets/scripts/grunt.py
@@ -5,9 +5,6 @@ Grunt.py contains details of the grunt enemy.
 import random
 import pygame
 
-# TODO: CREATE A FUNCTION FOR RESETTING/RESPAWNING THE ENEMY
-#       - Maybe add an attribute which dictates whether it can respawn?
-
 # TODO: ADD ADDITIONAL ATTRIBUTES
 #       - Is the enemy flying?
 #       - Is the enemy ground borne?
@@ -21,12 +18,14 @@ import pygame
 # TODO: MAKE THE GRUNT MOVE MORE TOWARD THE CENTRE OF THE SCREEN
 
 class Grunt(pygame.sprite.Sprite):
-    "The grunt jogs at the player and kills them once they reach them"
+    "The grunt is an enemy that deals melee damage to the enemy by reaching them"
     def __init__(self, game):
         self.width = 50
         self.height = 50
         self.set_surface()
 
+        self.dead = False
+        self.lives = 2
         self.flying = True
         self.running = True
         self.jog_speed = 0
@@ -39,7 +38,7 @@ class Grunt(pygame.sprite.Sprite):
 
         self.x_coord = random.randint(0, self.half_screen_width * 2)
         self.y_coord = random.randint(0, self.half_screen_height * 2)
-        self.z_coord = 0
+        self.z_coord = 50
 
     def events(self, event):
         "temnp"
@@ -51,45 +50,49 @@ class Grunt(pygame.sprite.Sprite):
             bullet_y = self.y_coord <= click_y <= self.y_coord + self.height
 
             if bullet_x and bullet_y:
-                self.width = 50
-                self.height = 50
-
-                screen_width = self.half_screen_width * 2
-                screen_height = self.half_screen_height * 2
-
-                self.x_coord = random.randint(0, screen_width - self.width)
-                self.y_coord = random.randint(0, screen_height - self.height)
-                self.z_coord = 0
-                self.speed = self.run_speed if self.running else self.jog_speed
+                if self.lives > 0:
+                    self.respawn()
+                    self.lives -= 1
+                else:
+                    self.dead = True
 
     def update(self, game):
         "update script for the enemy"
         # - Have the grunt move toward the centre of the screen, if it is smaller than the screen
-        if self.height < game.height and self.width < game.width:
-            self.speed += (self.width / 2) * game.delta_time
+        if not self.dead:
+            if self.height < game.height and self.width < game.width:
+                self.speed += (self.width / 2) * game.delta_time
 
-            if self.x_coord <= self.half_screen_width - self.width / 2:
-                self.x_coord += self.speed * game.delta_time
-            if self.x_coord >= self.half_screen_width - self.width / 2:
-                self.x_coord -= (self.speed * 2) * game.delta_time
-            if self.y_coord <= self.half_screen_height - self.height / 2:
-                self.y_coord += self.speed * game.delta_time
-            if self.y_coord >= self.half_screen_height - self.height / 2:
-                self.y_coord -= (self.speed * 2) * game.delta_time
+                if self.x_coord <= self.half_screen_width - self.width / 2:
+                    self.x_coord += self.speed * game.delta_time
+                if self.x_coord >= self.half_screen_width - self.width / 2:
+                    self.x_coord -= (self.speed * 2) * game.delta_time
+                if self.y_coord <= self.half_screen_height - self.height / 2:
+                    self.y_coord += self.speed * game.delta_time
+                if self.y_coord >= self.half_screen_height - self.height / 2:
+                    self.y_coord -= (self.speed * 2) * game.delta_time
 
-        # - Have the grunt increase in size until it fits the screen
-        if self.height < game.height:
-            self.height += self.speed * game.delta_time
-        if self.width < game.height:
-            self.width += self.speed * game.delta_time
+            # - Have the grunt increase in size until it fits the screen
+            if self.height < game.height:
+                self.height += self.speed * game.delta_time
+            if self.width < game.height:
+                self.width += self.speed * game.delta_time
 
-        self.set_surface()
+            self.set_surface()
+
+    def draw(self, surface):
+        "Draw the grunt onto the screen using the built in attributes"
+        surface.blit(self.image, [self.x_coord, self.y_coord])
 
     def set_surface(self):
         "Set the size of the grunt surface"
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill([255, 0, 0])
 
-    def draw(self, surface):
-        "Draw the grunt onto the screen using the built in attributes"
-        surface.blit(self.image, [self.x_coord, self.y_coord])
+    def respawn(self):
+        "Allows the grunt to respawn on the screen"
+        self.width = self.z_coord
+        self.height = self.z_coord
+        self.x_coord = random.randint(0, (self.half_screen_width * 2) - self.width)
+        self.y_coord = random.randint(0, (self.half_screen_height * 2) - self.height)
+        self.speed = self.run_speed if self.running else self.jog_speed
